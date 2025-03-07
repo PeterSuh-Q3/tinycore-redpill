a955af57a7fa0a780e15d861da10511ad08fdc1b
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-03-07 15:44:15 +0900

    Update functions_t.sh

--- functions_t.sh
+++ functions_t.sh
@@ -2423,22 +2423,19 @@ function savedefault {
     save_env --file \$prefix/grubenv saved_entry
     set gfxpayload=keep
     echo "TCRP-MSHELL JOT Version : ${rploaderver}"
-    echo "BUS Type: ${BUS}"
+    echo "BUS Type:   ${BUS}"
     echo -n "Boot Time: "; date
     echo ""
-    echo "Model   : ${MODEL}(${ORIGIN_PLATFORM})"
-    echo "Version : ${TARGET_VERSION}-${TARGET_REVISION}"
-    echo "Kernel  : ${KVER}"
-    echo "DMI     : $(dmesg 2>/dev/null | grep -i "DMI:" | head -1 | sed 's/\[.*\] DMI: //i')"
-    echo "CPU     : $(awk -F': ' '/model name/ {print $2}' /proc/cpuinfo | uniq)"
-    echo "MEM     : $(awk '/MemTotal:/ {printf "%.2f", $2 / 1024}' /proc/meminfo) MB"
-    echo ""    
+    echo "Model:   ${MODEL}(${ORIGIN_PLATFORM})"
+    echo "version: ${TARGET_VERSION}-${TARGET_REVISION}"
+    echo "kernel:  ${KVER}"
+    echo "DMI:     $(dmesg 2>/dev/null | grep -i "DMI:" | head -1 | sed 's/\[.*\] DMI: //i')"
+    echo "CPU:     $(awk -F': ' '/model name/ {print $2}' /proc/cpuinfo | uniq)"
+    echo "MEM:     $(awk '/MemTotal:/ {printf "%.2f", $2 / 1024}' /proc/meminfo) MB"
     echo "Cmdline:"
     echo "${USB_LINE}"
     echo ""
-    echo "IP Addr : $(IP} (Last Detected)"
-    echo ""
-    echo "Access http://find.synology.com/ or http://$(IP}:5000 to connect the DSM via web."
+    echo "Access http://find.synology.com/ to connect the DSM via web."
 }    
 EOF
 }
