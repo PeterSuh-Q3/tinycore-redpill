ff47308ea9e8ea4920798b5fa207bf194b3594ab
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-03-16 20:37:30 +0900

    Update menu_m.sh

--- menu_m.sh
+++ menu_m.sh
@@ -1720,7 +1720,7 @@ if [ "$FRKRNL" = "NO" ] && [ $(cat /mnt/${tcrppart}/cde/onboot.lst|grep rxvt | w
 fi
 
 #gettext
-curl -kLO# https://raw.githubusercontent.com/PeterSuh-Q3/tinycore-redpill/master/lang.tgz
+[ ! -f /home/tc/lang.tgz ] && curl -kLO# https://raw.githubusercontent.com/PeterSuh-Q3/tinycore-redpill/master/lang.tgz
 [ ! -d "/usr/local/share/locale" ] && sudo mkdir -p "/usr/local/share/locale"
 gunzip -c lang.tgz | sudo tar -xvf - -C /usr/local/share/locale
 locale
