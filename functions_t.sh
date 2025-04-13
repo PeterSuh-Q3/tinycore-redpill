e68f5e87e88770ccc3ba2040d8852a38f9a97f9b
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-04-13 14:57:13 +0900

    Update functions_t.sh

--- functions_t.sh
+++ functions_t.sh
@@ -3578,7 +3578,7 @@ function wr_part3() {
     fi   
 
     cd /mnt/${loaderdisk}3 && find . -name "*dsm*" -o -name "user_config.json" | sudo cpio -pdm "${mdiskpart}" 2>/dev/null
-    #sudo curl -kL# https://raw.githubusercontent.com/PeterSuh-Q3/tinycore-redpill/refs/heads/main/xtcrp.tgz -o "${mdiskpart}"/xtcrp.tgz
+    sudo curl -kL# https://raw.githubusercontent.com/PeterSuh-Q3/tinycore-redpill/refs/heads/main/xtcrp.tgz -o "${mdiskpart}"/xtcrp.tgz
     true
 }
 
