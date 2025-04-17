226a1fbce6bea3c13afa77266446dfdc2a12af87
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-04-17 23:20:04 +0900

    Update functions_t.sh

--- functions_t.sh
+++ functions_t.sh
@@ -3608,7 +3608,7 @@ function wr_part1() {
     sudo cp -vf /mnt/${loaderdisk}3/initrd-friend  "${mdiskpart}"
 
     sudo mkdir -p /usr/local/share/locale
-    sudo grub-install --target=x86_64-efi --boot-directory="${mdiskpart}"/boot --efi-directory="${mdiskpart}" --removable > /dev/null 2>&1
+    sudo grub-install --target=x86_64-efi --boot-directory="${mdiskpart}"/boot --efi-directory=/boot/efi --removable > /dev/null 2>&1
     #[ $? -ne 0 ] && returnto "excute grub-install ${mdiskpart} failed. Stop processing!!! " && false
     sudo grub-install --target=i386-pc --boot-directory="${mdiskpart}"/boot "${edisk}" > /dev/null 2>&1
     #[ $? -ne 0 ] && returnto "excute grub-install ${mdiskpart} failed. Stop processing!!! " && false
