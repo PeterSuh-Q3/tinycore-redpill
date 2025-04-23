9a4120b5b25d779060632af1caa2baa0c65a6385
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-04-23 13:09:37 +0900

    Update functions_t.sh

--- functions_t.sh
+++ functions_t.sh
@@ -21,7 +21,7 @@ mshtarfile="https://raw.githubusercontent.com/PeterSuh-Q3/tinycore-redpill/maste
 #Defaults
 smallfixnumber="0"
 
-kver3platforms="bromolow braswell avoton cedarview"
+kver3platforms="bromolow braswell avoton cedarview grantley"
 
 #Check if FRIEND kernel exists
 if [[ "$(uname -a | grep -c tcrpfriend)" -gt 0 ]]; then
@@ -872,7 +872,7 @@ function getvarsmshell()
     MODELS_JSON="/home/tc/models.json"
 
     # Define platform groups
-    platforms="epyc7002 broadwellnk broadwell bromolow broadwellnkv2 broadwellntbap purley denverton apollolake r1000 v1000 geminilake avoton braswell cedarview"
+    platforms="epyc7002 broadwellnk broadwell bromolow broadwellnkv2 broadwellntbap purley denverton apollolake r1000 v1000 geminilake avoton braswell cedarview grantley"
 
     # Initialize MODELS array
     MODELS=()
