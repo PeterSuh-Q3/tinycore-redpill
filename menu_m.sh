b59bef0c713a98c0512fec7702c805188c91653f
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-05-12 15:56:42 +0900

    Update menu_m.sh

--- menu_m.sh
+++ menu_m.sh
@@ -1413,14 +1413,14 @@ function changesatadom() {
   [ -z "${resp}" ] && return
   satadom_edit "${resp}"
   
-  #SATADOMRES="${resp}"
-  #if [ "${SATADOMRES}" = "0" ]; then
-  #  DOMKIND="Disable"
-  #elif [ "${SATADOMRES}" = "1" ]; then
-  #  DOMKIND="Native"
-  #else
-  #  DOMKIND="Fake"
-  #fi
+  SATADOMRES="${resp}"
+  if [ "${SATADOMRES}" = "0" ]; then
+    DOMKIND="Disable"
+  elif [ "${SATADOMRES}" = "1" ]; then
+    DOMKIND="Native"
+  else
+    DOMKIND="Fake"
+  fi
 }
 
 function additional() {
