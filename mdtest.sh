2b4e99c41f153770e98801bd50b9cea0475ae3da
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-05-22 15:54:34 +0900

    Update mdtest.sh

--- mdtest.sh
+++ mdtest.sh
@@ -19,8 +19,21 @@ sudo mount -t "${T:-ext4}" /dev/md0 "${TMP_PATH}/mdX"
 
 sudo ls -l ${TMP_PATH}/mdX/etc/shadow
 
+  if [ -f "${TMP_PATH}/mdX/etc/shadow" ]; then
+    while read -r L; do
+      U=$(echo "${L}" | awk -F ':' '{if ($2 != "*" && $2 != "!!") print $1;}')
+      [ -z "${U}" ] && continue
+      E=$(echo "${L}" | awk -F ':' '{if ($8 == "1") print "disabled"; else print "        ";}')
+      grep -q "status=on" "${TMP_PATH}/mdX/usr/syno/etc/packages/SecureSignIn/preference/${U}/method.config" 2>/dev/null
+      [ $? -eq 0 ] && S="SecureSignIn" || S="            "
+      printf "\"%-36s %-10s %-14s\"\n" "${U}" "${E}" "${S}" >>"${TMP_PATH}/menuz"
+    done <<<"$(sudo cat "${TMP_PATH}/mdX/etc/shadow" 2>/dev/null)"
+  fi
+
 sudo umount "${TMP_PATH}/mdX"
 sudo mdadm --stop /dev/md0
 sudo rm -rf "${TMP_PATH}/mdX"
+
+sudo cat ${TMP_PATH}/menuz
 
 echo "Finished!"
