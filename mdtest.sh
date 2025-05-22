51e3564de5deb48f03e51f90580e7a2e2f6a304b
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-05-22 15:03:50 +0900

    Update mdtest.sh

--- mdtest.sh
+++ mdtest.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 TMP_PATH=/tmp
-FRKRNL=NO
 
 [ "$(which mdadm)_" == "_" ] && tce-load -iw mdadm
 DSMROOTS="$(sudo fdisk -l | grep -E "sd[a-z]{1,2}1" | grep "Linux raid autodetect" | grep -E '16785407|4982527' | awk '{print $1}')"
@@ -12,7 +11,7 @@ sudo mkdir -p "${TMP_PATH}/mdX"
 num=$(echo $DSMROOTS | wc -w)
 sudo mdadm -C /dev/md0 -e 0.9 -amd -R -l1 --force -n$num $DSMROOTS
 T="$(sudo blkid -o value -s TYPE /dev/md0"
-[ "$FRKRNL" = "NO" ] && sudo tune2fs -O ^quota /dev/md0
+sudo tune2fs -O ^quota /dev/md0
 sleep 2
 sudo mount -t "${T:-ext4}" /dev/md0 "${TMP_PATH}/mdX"
 
