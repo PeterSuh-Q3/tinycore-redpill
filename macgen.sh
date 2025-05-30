b1f1fc4cab9f87d324df4a205c2083270ebfbb97
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-05-31 00:20:24 +0900

    Update macgen.sh

--- macgen.sh
+++ macgen.sh
@@ -1,7 +1,7 @@
 function generateMacAddress() {
     #toupper "Mac Address: 00:11:32:$(randomhex):$(randomhex):$(randomhex)"
-    macprefixmodels="DS923+ DS1522+ RS4021xs+ HD6500 RS2423RP+ RS4021xs+ SA3410"
-    if [ $(echo ${macprefixmodels} | grep ${1} | wc -l ) -gt 0 ]; then
+    macprefixmodels="DS923+ DS925+ DS1522+ HD6500 RS2423RP+ RS4021xs+ SA3410"
+    if echo ${macprefixmodels} | grep -qw ${1}; then
         # DS1522xs+ and DS923+ Mac starts with 90:09:D0
         printf '90:09:D0:%02X:%02X:%02X' $((RANDOM % 256)) $((RANDOM % 256)) $((RANDOM % 256))
     else
