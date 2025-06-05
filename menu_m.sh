0ade00513923857291624559d5a97417bdcb0d66
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-06-05 22:17:58 +0900

    Update menu_m.sh

--- menu_m.sh
+++ menu_m.sh
@@ -996,7 +996,9 @@ function writexsession() {
   else
     echo "'ttyd' pattern already exists in /opt/bootlocal.sh"
     sudo sed -i "/ttyd/d" /opt/bootlocal.sh
+    sudo sed -i "/mountvol/d" /opt/bootlocal.sh
     echo 'sudo /home/tc/ttyd login -f tc 2>/dev/null &' >> /opt/bootlocal.sh
+    echo '[ $(/bin/uname -r | /bin/grep 4.14.10 | /usr/bin/wc -l) -eq 1 ] && /home/tc/mountvol.sh &' >> /opt/bootlocal.sh
   fi
 
 }
