c40b81ace548960d4269d6eb740a9718d3287064
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-06-05 21:09:47 +0900

    Update functions_t.sh

--- functions_t.sh
+++ functions_t.sh
@@ -1440,6 +1440,8 @@ function get_tinycore9() {
       echo "tinycore 9.0 md5 check is OK! ( corepure64.gz / vmlinuz64 ) "
       sudo curl -kL# https://raw.githubusercontent.com/PeterSuh-Q3/tinycore-redpill/master/tinycore_9.0/cde.tgz -o /mnt/${tcrppart}/v9/cde.tgz
       sudo tar -zxvf /mnt/${tcrppart}/v9/cde.tgz --no-same-owner -C /mnt/${tcrppart}/v9/cde
+      sudo curl -kL# https://raw.githubusercontent.com/PeterSuh-Q3/tinycore-redpill/master/mountvol.sh -o /home/tc/mountvol.sh
+      echo "/home/tc/mountvol.sh" >> /opt/bootlocal.sh
       sudo sed -i "/set default=/cset default=\"4\"" /mnt/${loaderdisk}1/boot/grub/grub.cfg
       echo 'Y'|rploader backup
       restart
