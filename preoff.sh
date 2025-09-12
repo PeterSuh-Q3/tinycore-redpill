e168b3920b634acac95c329912c4215d04563edf
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-09-12 19:07:57 +0900

    Update preoff.sh

--- preoff.sh
+++ preoff.sh
@@ -11,8 +11,14 @@ echo "tcrppart: $tcrppart"
 
 cd /mnt/$tcrppart/
 
-mv -vrf /dev/shm/tcrp-addons/ /mnt/$tcrppart/.
+mv -vf /dev/shm/tcrp-addons/ /mnt/$tcrppart/.
+
+sudo rm /mnt/$tcrppart/xtcrp.tgz
+
+sudo rm -rf /mnt/$tcrppart/redpill-load
 git clone -b master --single-branch --depth=1 https://github.com/PeterSuh-Q3/redpill-load.git
+
+sudo rm -rf /mnt/$tcrppart/tcrp-modules
 
 mkdir -p ./tcrp-modules/all-modules/releases
 mkdir -p ./tcrp-modules/all-modules/src
@@ -20,16 +26,16 @@ mkdir -p ./tcrp-modules/all-modules/src
 mkdir -p ./tcrp-modules/eudev/releases
 mkdir -p ./tcrp-modules/eudev/recipes
 
-curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/rpext-index.json -o ./tcrp-modules/all-modules
-curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/releases/firmware.tgz -o ./tcrp-modules/all-modules/releases
-curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/releases/firmwarei915.tgz -o ./tcrp-modules/all-modules/releases
-curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/releases/epyc700272.json -o ./tcrp-modules/all-modules/releases
-curl -kL https://github.com/PeterSuh-Q3/arpl-modules/releases/download/${tag}/epyc7002-7.2-5.10.55.tgz -o ./tcrp-modules/all-modules/releases
+curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/rpext-index.json -o ./tcrp-modules/all-modules/rpext-index.json
+curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/releases/firmware.tgz -o ./tcrp-modules/all-modules/releases/firmware.tgz
+curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/releases/firmwarei915.tgz -o ./tcrp-modules/all-modules/releases/firmwarei915.tgz
+curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/releases/epyc700272.json -o ./tcrp-modules/all-modules/releases/epyc700272.json
+curl -kL https://github.com/PeterSuh-Q3/arpl-modules/releases/download/${tag}/epyc7002-7.2-5.10.55.tgz -o ./tcrp-modules/all-modules/releases/epyc7002-7.2-5.10.55.tgz
 
-curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/eudev/rpext-index.json -o ./tcrp-modules/eudev
-curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/eudev/releases/eudev-7.1.tgz -o ./tcrp-modules/eudev/releases
-curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/eudev/releases/install.sh -o ./tcrp-modules/eudev/releases
-curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/eudev/recipes/universal.json -o ./tcrp-modules/eudev/recipes
+curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/eudev/rpext-index.json -o ./tcrp-modules/eudev/rpext-index.json
+curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/eudev/releases/eudev-7.1.tgz -o ./tcrp-modules/eudev/releases/eudev-7.1.tgz
+curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/eudev/releases/install.sh -o ./tcrp-modules/eudev/releases/install.sh
+curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/eudev/recipes/universal.json -o ./tcrp-modules/eudev/recipes/universal.json
 
 sha256=$(sha256sum ./tcrp-modules/all-modules/releases/epyc7002-7.2-5.10.55.tgz
 org=$(jq -r '.files[0].sha256' ./tcrp-modules/all-modules/releases/epyc700272.json)
