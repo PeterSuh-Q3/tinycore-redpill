0950f2a5b935ef82ddbc8f2b478789bcf60f70cd
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-09-14 16:10:22 +0900

    Add firmware download and comment out old code
    Added a new firmware download and commented out the previous firmware processing code.

--- preoff.sh
+++ preoff.sh
@@ -31,10 +31,11 @@ mkdir -p ./tcrp-modules/ddsml/recipes
 
 curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/rpext-index.json -o ./tcrp-modules/all-modules/rpext-index.json
 curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/releases/epyc700272.json -o ./tcrp-modules/all-modules/releases/epyc700272.json
+curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/releases/firmware.tgz -o ./tcrp-modules/all-modules/releases/firmware.tgz
 curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/releases/firmwarei915.tgz -o ./tcrp-modules/all-modules/releases/firmwarei915.tgz
 curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/all-modules/src/install.sh -o ./tcrp-modules/all-modules/src/install.sh
 curl -kL https://github.com/PeterSuh-Q3/arpl-modules/releases/download/${tag}/epyc7002-7.2-5.10.55.tgz -o ./tcrp-modules/all-modules/releases/epyc7002-7.2-5.10.55.tgz
-curl -kL https://github.com/PeterSuh-Q3/arpl-modules/releases/download/rr-last/firmware.tgz  -o ./tcrp-modules/all-modules/releases/firmware.tgz 
+#curl -kL https://github.com/PeterSuh-Q3/arpl-modules/releases/download/rr-last/firmware.tgz  -o ./tcrp-modules/all-modules/releases/firmware.tgz 
 
 curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/eudev/rpext-index.json -o ./tcrp-modules/eudev/rpext-index.json
 curl -kL https://github.com/PeterSuh-Q3/tcrp-modules/raw/refs/heads/main/eudev/recipes/universal.json -o ./tcrp-modules/eudev/recipes/universal.json
@@ -51,7 +52,7 @@ sha256=$(sha256sum ./tcrp-modules/all-modules/releases/epyc7002-7.2-5.10.55.tgz 
 org=$(jq -r '.files[0].sha256' ./tcrp-modules/all-modules/releases/epyc700272.json)
 sudo sed -i "s/$org/$sha256/g" ./tcrp-modules/all-modules/releases/epyc700272.json
 
-sha256=$(sha256sum ./tcrp-modules/all-modules/releases/firmware.tgz | awk '{print $1}')
-org=$(jq -r '.files[1].sha256' ./tcrp-modules/all-modules/releases/epyc700272.json)
-sudo sed -i "s/$org/$sha256/g" ./tcrp-modules/all-modules/releases/epyc700272.json
+#sha256=$(sha256sum ./tcrp-modules/all-modules/releases/firmware.tgz | awk '{print $1}')
+#org=$(jq -r '.files[1].sha256' ./tcrp-modules/all-modules/releases/epyc700272.json)
+#sudo sed -i "s/$org/$sha256/g" ./tcrp-modules/all-modules/releases/epyc700272.json
 
