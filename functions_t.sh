b0d16abf0bedb97a47d4e832cc3a6cd9968568eb
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-10-24 18:03:50 +0900

    Update rploaderver to version 1.2.6.2

--- functions_t.sh
+++ functions_t.sh
@@ -2,7 +2,7 @@
 
 set -u # Unbound variable errors are not allowed
 
-rploaderver="1.2.6.1"
+rploaderver="1.2.6.2"
 build="master"
 redpillmake="prod"
 
@@ -199,7 +199,8 @@ function history() {
     1.2.5.6 Added udma-crc-check Addon for Telegram alarm when S.M.A.R.T UDMA CRC Count (ID 199) increases
     1.2.5.7 Dramatically improved USB backup speed
     1.2.6.0 Add Support DSM 7.3.0-81180 Official Version (For kernel 4.4-based use only)
-    1.2.6.1 Loader image size is distributed in two sizes: 2GB and 4GB.
+    1.2.6.1 Loader image size is distributed in two sizes: 2GB and 4GB
+    1.2.6.2 When changing user_config.json, process cmd_line at once without loader build
     --------------------------------------------------------------------------------------
 EOF
 }
@@ -590,7 +591,9 @@ EOF
 # 2025.10.08 v1.2.6.0 
 # Add Support DSM 7.3.0-81180 Official Version (For kernel 4.4-based use only)
 # 2025.10.22 v1.2.6.1 
-# Loader image size is distributed in two sizes: 2GB and 4GB.
+# Loader image size is distributed in two sizes: 2GB and 4GB
+# 2025.10.24 v1.2.6.2 
+# When changing user_config.json, process cmd_line at once without loader build
     
 function showlastupdate() {
     cat <<EOF
@@ -657,7 +660,10 @@ function showlastupdate() {
 # Add Support DSM 7.3.0-81180 Official Version (For kernel 4.4-based use only)
 
 # 2025.10.22 v1.2.6.1 
-# Loader image size is distributed in two sizes: 2GB and 4GB.
+# Loader image size is distributed in two sizes: 2GB and 4GB
+
+# 2025.10.24 v1.2.6.2 
+# When changing user_config.json, process cmd_line at once without loader build
 
 EOF
 }
