76e3eb154433b9d6205f9b4fec371718a53923a8
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-11-19 00:08:39 +0900

    Add warning prompt for version build confirmation
    Added a warning prompt before continuing the build process.

--- functions_t.sh
+++ functions_t.sh
@@ -5349,6 +5349,12 @@ function my() {
       msgalert "DSM 7.3 또는 7.3.1 로더빌드 기능은 정식 lkm 이 출시되기전까지 임시로 사용할 수 있는 시험적인 기능입니다.\n"
       msgalert "이미 DSM 7.3 이상을 시노디스크에 미리 설치한 경우만 기능을 허용합니댜.\n"
       msgalert "이 임시기능은 네트워크 무반응, 시노디스크 사라짐 현상을 동반할 수 있으므로 주의하시기 바랍니다.\n"
+      
+      echo -n "(Warning) Do you want to continue building this version? [yY/nN] : "
+      readanswer
+      if [ "${answer}" = "N" ] || [ "${answer}" = "n" ]; then
+          exit 0
+      fi
      
       if chkDsmversion; then
           echo "[OK] DSM version matched. Proceeding."
