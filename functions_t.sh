f2ca02a13aae4910ce1f5ad32c0b949b4d1b2a0a
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-12-04 22:06:59 +0900

    Simplify pats.json download logic
    Refactor downloading logic for pats.json files based on machine type and hypervisor.

--- functions_t.sh
+++ functions_t.sh
@@ -5588,6 +5588,21 @@ function my() {
   echo "BUILD is $BUILD"
   
   cecho p "DSM PAT file pre-downloading in progress..."
+  
+  echo  "download original pats.json file..."
+  pats_url="https://raw.githubusercontent.com/PeterSuh-Q3/redpill-load/master/config/pats.json"
+  pats_t_url="https://raw.githubusercontent.com/PeterSuh-Q3/redpill-load/master/config/pats_t.json"
+  if [ "$MACHINE" = "VIRTUAL" ] && [ "$HYPERVISOR" = "KVM" ]; then
+    curl -skL# $pats_t_url -o $configfile
+  else  
+    if [ -f /tmp/test_mode ]; then
+        cecho g "###############################  This is Test Mode  ############################"        
+        curl -skL# $pats_t_url -o $configfile
+    else
+        [ "${BUS}" == "block" ] && curl -skL# $pats_t_url -o $configfile || curl -skL# $pats_url -o $configfile
+    fi    
+  fi  
+
   URL=$(jq -e -r ".\"${MODEL}\" | to_entries | map(select(.key | startswith(\"${BUILD}\"))) | map(.value.url) | .[0]" "${configfile}")
   cecho y "$URL"
   if [[ $BIOS_CNT -eq 1 ]] && [ "$FRKRNL" = "YES" ]; then 
@@ -5645,19 +5660,6 @@ function my() {
       #    exit 99
       #  fi
       #else
-      echo  "download original pats.json file..."
-      pats_url="https://raw.githubusercontent.com/PeterSuh-Q3/redpill-load/master/config/pats.json"
-      pats_t_url="https://raw.githubusercontent.com/PeterSuh-Q3/redpill-load/master/config/pats_t.json"
-      if [ "$MACHINE" = "VIRTUAL" ] && [ "$HYPERVISOR" = "KVM" ]; then
-        curl -skL# $pats_t_url -o $configfile
-      else  
-        if [ -f /tmp/test_mode ]; then
-            cecho g "###############################  This is Test Mode  ############################"        
-            curl -skL# $pats_t_url -o $configfile
-        else
-            [ "${BUS}" == "block" ] && curl -skL# $pats_t_url -o $configfile || curl -skL# $pats_url -o $configfile
-        fi    
-      fi  
       echo "offline = ${offline}"
         [ "${offline}" = "NO" ] && _pat_process    
       #fi
