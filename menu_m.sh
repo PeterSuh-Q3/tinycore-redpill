3558bd4457ffff35a812c0ee10edf00d00b747fa
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2026-02-19 20:47:06 +0900

    Log R8168_YN and platform information to buildstatus

--- menu_m.sh
+++ menu_m.sh
@@ -984,6 +984,7 @@ function make() {
 
   getip
   setSuggest $MODEL
+  echo "R8168_YN=${R8168_YN} : kver5explatforms=$kver5explatforms} : platform=${platform}" >> /home/tc/buildstatus
   if [ "${R8168_YN}" = "Y" ] && echo "${kver5explatforms}" | grep -qw "${platform}"; then
     DMPM="DDSML+EUDEV"
   fi
