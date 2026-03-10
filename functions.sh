b28202a6900166ce992164219df3f8d1f41a79a0
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2026-03-10 15:06:20 +0900

    Change compression method for uncompressed ramdisk

--- functions.sh
+++ functions.sh
@@ -4343,8 +4343,8 @@ EOF
     if [ "$RD_COMPRESSED" = "false" ]; then
         if [ "$FRKRNL" = "NO" ]; then
             if [ "${MDLNAME}" == "custom-modules" ]; then
-                echo "Ramdisk in not compressed, use bsdcpio + zstd -T0 -19"            
-                (cd $rdtemp && sudo find . | sudo bsdcpio -o -H newc -R root:root | zstd -c -T0 -19 > /mnt/${loaderdisk}3/initrd-dsm) >/dev/null
+                echo "Ramdisk in not compressed, use bsdcpio + xz -6 --format=lzma"
+                (cd $rdtemp && sudo find . | sudo bsdcpio -o -H newc -R root:root | xz -6 --format=lzma > /mnt/${loaderdisk}3/initrd-dsm) >/dev/null
             else
                 echo "Ramdisk in not compressed, use cpio raw"            
                 (cd $rdtemp && sudo find . | sudo cpio -o -H newc -R root:root > /mnt/${loaderdisk}3/initrd-dsm) >/dev/null
