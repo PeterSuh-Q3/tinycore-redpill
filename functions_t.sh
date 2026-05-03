9b5c6900fa65600f3041043505a384f0ada57f2f
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2026-05-03 14:37:09 +0900

    Update dsm6notsupported platforms list
    Removed unsupported platforms from dsm6notsupported variable.

--- functions_t.sh
+++ functions_t.sh
@@ -24,7 +24,7 @@ smallfixnumber="0"
 kver3platforms="bromolow braswell avoton cedarview"
 kver5platforms="epyc7002 v1000nk r1000nk geminilakenk"
 nosas5platforms="bromolow broadwellntbap broadwellnkv2 purley"
-dsm6notsupported="geminilake v1000 purley braswell denverton broadwellntbap"
+dsm6notsupported="broadwellntbap"
 
 # 전역 변수로 플래그 설정 (한번 Y면 영구 유지)
 R8168_YN="N"
