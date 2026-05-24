6164def71e26f3c88ee47e4ea965bd4a394ccca8
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2026-05-25 00:13:03 +0900

    Notify if tc-functions file is missing
    Add error message for missing tc-functions file

--- menu_m.sh
+++ menu_m.sh
@@ -10,6 +10,7 @@ set -u # Unbound variable errors are not allowed
 export PATH='/home/tc/.local/bin:/usr/local/sbin:/usr/local/bin:/apps/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 
 if [ ! -f "/etc/init.d/tc-functions" ]; then
+  echo "/etc/init.d/tc-functions is missing recover file..."
   sudo /usr/local/bin/curl -kL https://raw.githubusercontent.com/tinycorelinux/Core-scripts/refs/heads/master/etc/init.d/tc-functions -o /etc/init.d/tc-functions
   source /etc/init.d/tc-functions
   sudo filetool.sh -b 
