aced3b9a6f2695f767fc4a09d166c4cef498a590
PeterSuh-Q3 <dante9000@gmail.com>
2026-07-14 15:47:25 +0900

    functions.sh: monitor()의 Swap Usage division by zero 수정
    swap이 구성되지 않은 환경(diskless Alpine 로더 대부분)에서 free의 Swap
    total이 0이 되어 awk의 \$3/\$2*100 계산이 "division by zero attempted"로
    죽던 문제. 분모가 0이면 0%로 처리하도록 삼항 가드 추가.

--- functions.sh
+++ functions.sh
@@ -2998,7 +2998,7 @@ function monitor() {
         grep -i menuentry /mnt/${loaderdisk}1/boot/grub/grub.cfg | awk -F \' '{print $2}'
         echo -e "-------------------------------CPU / Memory----------------------------------"
         msgnormal "Total Memory (MB):\t"$(cat /proc/meminfo |grep MemTotal | awk '{printf("%.2f"), $2/1000}')
-        echo -e "Swap Usage:\t\t"$(free | awk '/Swap/{printf("%.2f%"), $3/$2*100}')
+        echo -e "Swap Usage:\t\t"$(free | awk '/Swap/{printf("%.2f%"), ($2>0 ? $3/$2*100 : 0)}')
         echo -e "CPU Usage:\t\t"$(cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' | awk '{print $0}' | head -1)
         echo -e "-------------------------------Disk Usage >80%-------------------------------"
         df -Ph /mnt/${loaderdisk}1 /mnt/${loaderdisk}2 /mnt/${loaderdisk}3
